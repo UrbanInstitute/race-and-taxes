16084514f7fa840ab909bc30880d8c14002d22e4
Carman <jstraley@gmail.com>
2019-12-19 11:33:15 -0500

    accordions and hover coordination all functional

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,63 @@
-console.log('\'Allo \'Allo!');
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+//TODO - adjust height of the page when you swtich categories. Must need to add height for hte header to the jQuery whatever it does
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+var spacingUnit = 60;
 
 $(document).ready(function() {
-    var anchor = window.location.hash + "-section"
-    $(anchor).collapse("toggle")
+    var section = window.location.hash,
+      anchor = section + "-section",
+      divHeader = section + "-info";
+
+    $(anchor).collapse("toggle");
+
+    //the form hilites need to match the open dropdown section
+    if (window.location.hash != ""){
+      hiliteForm(section, "active-click")
+      $(divHeader).toggleClass("active-click")
+    }
+
+    var headerHeight = $("#header-pinned").outerHeight();
+    $("div.title-content").css("margin-top", headerHeight + spacingUnit);
 });
 
-$(".main-form rect").addClass("inactive")
-$(".main-form text").css("display", "none")
+function hiliteForm(sectionID, newClass){
+  var selectedRects = sectionID + " > g > rect",
+      selectedText = sectionID + " > g > text";
 
+  $(selectedRects + ", " + selectedText).toggleClass(newClass);
+}
 
-$(".st0").mouseenter(
-	function() {
-    $(this).removeClass()
-    $(this).addClass("selected")
-  })
+$(".card-header").mouseenter(function(){
+  var formCat = "#" + this.getAttribute("data-cat");
+  hiliteForm(formCat, "active-hover");
+  $(this).addClass("active-hover")
+})
 
-$(".st0").mouseleave(
-	function() {
-    $(this).removeClass()
-    $(this).addClass("st0")
-  })
+$(".card-header").mouseleave(function(){
+  var formCat = "#" + this.getAttribute("data-cat");
+  hiliteForm(formCat, "active-hover");
+  $(this).removeClass("active-hover");
+})
+
+$(".btn-link").on("click", function(){
+  var formCat = this.getAttribute("data-target").replace("-section", ""),
+      selectedRects = formCat + " > g > rect",
+      selectedText = formCat + " > g > text",
+      drawerOpen = $(this.parentElement.parentElement.parentElement.parentElement.lastElementChild).hasClass("show"); 
+
+  $("div.card-header").removeClass("active-click");
+  $("text.active-click").removeClass("active-click");
+  $("rect.active-click").removeClass("active-click");
+
+  if (drawerOpen){
+    $(selectedRects + ", " + selectedText).removeClass("active-click");
+
+  } else {
+    $(selectedRects + ", " + selectedText).addClass("active-click");
+    $(this.parentElement.parentElement.parentElement).addClass("active-click");
+  }
+
+})
+
 
 
