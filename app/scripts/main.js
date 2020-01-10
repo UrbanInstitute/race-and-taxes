7dbf8fbc6b09816cc11f73b0614a7c45eabdf410
Carman <jstraley@gmail.com>
2020-01-10 10:09:45 -0500

    not using scroll behavior at all

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,128 +3,180 @@
 // social intents
 // links for both
 // how do you serve fonts here?
+// IE - viewport on SVG is wrong
+// Safari - does not show SVG via img tag, use 'object'
 
-var spacingUnit = 60;
+
 
 $(document).ready(function() {
-    var section = window.location.hash,
-      anchor = section + '-section',
-      divHeader = section + '-info',
-      box = section + '-boxes';
+  var spacingUnit = 60;
 
-    $(anchor).collapse('toggle');
+  var section = window.location.hash,
+    anchor = section + '-section',
+    divHeader = section + '-info',
+    box = section + '-boxes';
 
-    //the form hilites need to match the open dropdown section
-    if (window.location.hash != ''){
-      $(box).children().children().addClass('active-click');
-      $(divHeader).toggleClass('active-click');
-      $(divHeader + ' > span' ).addClass('rotate-arrow-to-up');
+  $(anchor).collapse('toggle');
+
+  //the form hilites need to match the open dropdown section
+  if (window.location.hash != ''){
+    $(box).children().children().addClass('active-click');
+    $(divHeader).toggleClass('active-click');
+    $(divHeader + ' > span' ).addClass('rotate-arrow-to-up');
+  }
+
+  var headerHeight = $('#header-pinned').outerHeight();
+  $('div.title-content').css('margin-top', headerHeight + spacingUnit);
+  
+  //h/t: https://stackoverflow.com/questions/49636727/why-is-scroll-behaviorsmooth-not-working-but-javascript-window-scroll-smooth-is?newreg=472532a8017f4bc59a4965f7dcf5a84b
+  window.smoothScrollTo = function(endX, endY, duration) {
+    let startX = window.scrollX || window.pageXOffset,
+    startY = window.scrollY || window.pageYOffset,
+    distanceX = endX - startX,
+    distanceY = endY - startY,
+    startTime = new Date().getTime();
+    console.log('start ' + startY + ' end ' + endY)
+    // Easing function
+    let easeInOutQuart = function(time, from, distance, duration) {
+        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    };
+
+    let timer = window.setInterval(function() {
+        let time = new Date().getTime() - startTime,
+        newX = easeInOutQuart(time, startX, distanceX, duration),
+        newY = easeInOutQuart(time, startY, distanceY, duration);
+        if (time >= duration) {
+            window.clearInterval(timer);
+        }
+        window.scrollTo(newX, newY);
+    }, 1000 / 60); // 60 fps
+
+  };
+
+  //*********MOUSEOVER **********//
+  function mouseoverHilite(sectionID){
+    var lineItemG = sectionID + '-boxes',
+        cardHeader = sectionID + '-info';
+
+    $(lineItemG).children().children().toggleClass('active-hover');
+    $('div.card-header').removeClass('active-hover');
+    $(cardHeader).addClass('active-hover');
+  };
+
+  $('.card-header').mouseenter(function(){
+    var lineItem = '#' + this.getAttribute('data-box').replace('-boxes','');
+    mouseoverHilite(lineItem);
+  });
+
+  $('.card-header').mouseleave(function(){
+    var lineItem = '#' + this.getAttribute('data-box').replace('-boxes','');
+    mouseoverHilite(lineItem);
+    $(this).removeClass('active-hover');
+  });
+
+  //highlight form box on mouseover and highlight corresponding drawer
+  $('g#line-items > g > g').mouseenter(function(){
+    var lineItem = '#' + $(this.parentElement).attr('id').replace('-boxes', '');
+    mouseoverHilite(lineItem);
+  });
+
+  $('g#line-items > g > g').mouseleave(function(){
+    var lineItem = '#' + $(this.parentElement).attr('id').replace('-boxes', ''),
+      cardHeader = lineItem + '-info';
+    mouseoverHilite(lineItem);
+    $(cardHeader).removeClass('active-hover');
+  });
+
+  //underline the text on mouseover
+  //note: .st4 = bold text, .st6 = light text
+  $('g#line-items > g > g > text').mouseenter(function() {
+    var formCat = $(this.parentElement.parentElement).attr('id');
+    $('#' + formCat + '> g').css('text-decoration', 'underline')
+  });
+
+  $('g#line-items > g > g > text').mouseleave(function() {
+    var formCat = $(this.parentElement.parentElement).attr('id');
+    $('#' + formCat + '> g').css('text-decoration', 'none')
+  });
+
+
+  //*********CLICK ********//
+  function clickHilite(sectionID){
+    
+      var lineItemG = sectionID + '-boxes',
+        cardHeader = sectionID + '-info',
+        section = sectionID + '-section',
+        isAnchorClick = $(event.currentTarget).hasClass('btn'),
+        drawerOpen = $(section).hasClass('show'); 
+
+    if(!isAnchorClick){
+      $(sectionID + '-section').collapse('toggle');
     }
 
-    var headerHeight = $('#header-pinned').outerHeight();
-    $('div.title-content').css('margin-top', headerHeight + spacingUnit);
-
-});
-
-//*********MOUSEOVER **********//
-function mouseoverHilite(sectionID){
-  var lineItemG = sectionID + '-boxes',
-      cardHeader = sectionID + '-info';
-
-  $(lineItemG).children().children().toggleClass('active-hover');
-  $('div.card-header').removeClass('active-hover');
-  $(cardHeader).addClass('active-hover');
-};
-
-$('.card-header').mouseenter(function(){
-  var lineItem = '#' + this.getAttribute('data-box').replace('-boxes','');
-  mouseoverHilite(lineItem);
-});
-
-$('.card-header').mouseleave(function(){
-  var lineItem = '#' + this.getAttribute('data-box').replace('-boxes','');
-  mouseoverHilite(lineItem);
-  $(this).removeClass('active-hover');
-});
-
-//highlight form box on mouseover and highlight corresponding drawer
-$('g#line-items > g > g').mouseenter(function(){
-  var lineItem = '#' + $(this.parentElement).attr('id').replace('-boxes', '');
-  mouseoverHilite(lineItem);
-});
-
-$('g#line-items > g > g').mouseleave(function(){
-  var lineItem = '#' + $(this.parentElement).attr('id').replace('-boxes', ''),
-    cardHeader = lineItem + '-info';
-  mouseoverHilite(lineItem);
-  $(cardHeader).removeClass('active-hover');
-});
-
-//underline the text on mouseover
-//note: .st4 = bold text, .st6 = light text
-$('g#line-items > g > g > text').mouseenter(function() {
-  var formCat = $(this.parentElement.parentElement).attr('id');
-  $('#' + formCat + '> g > text').css('text-decoration', 'underline')
-});
-
-$('g#line-items > g > g > text').mouseleave(function() {
-  var formCat = $(this.parentElement.parentElement).attr('id');
-  $('#' + formCat + '> g > text').css('text-decoration', 'none')
-});
-
-
-//*********CLICK ********//
-function clickHilite(sectionID, event){
-  event.preventDefault();
-    var lineItemG = sectionID + '-boxes',
-      cardHeader = sectionID + '-info',
-      section = sectionID + '-section',
-      isAnchorClick = $(event.currentTarget).hasClass('btn'),
-      drawerOpen = $(section).hasClass('show'); 
-
-  if(!isAnchorClick){
-    $(sectionID + '-section').collapse('toggle');
-  }
-
-  //deselect all card-headers & hilite selected
-  $('div.card-header').removeClass('active-click');
-  $(cardHeader).addClass('active-click');
-
-  //rotate any turned arrow back to original down position
-  $('div.card-header > span' ).removeClass('rotate-arrow-to-up');
-  //deselect boxes on form
-  $('g#line-items > g > g').children().removeClass('active-click');
-
-  if (drawerOpen){
-    //deselect card headers
+    //deselect all card-headers & hilite selected
     $('div.card-header').removeClass('active-click');
-  } else {
-    $(lineItemG).children().children().addClass('active-click');
-    $(cardHeader + ' > span' ).addClass('rotate-arrow-to-up');
+    $(cardHeader).addClass('active-click');
+
+    //rotate any turned arrow back to original down position
+    $('div.card-header > span' ).removeClass('rotate-arrow-to-up');
+    //deselect boxes on form
+    $('g#line-items > g > g').children().removeClass('active-click');
+
+    if (drawerOpen){
+      $('div.card-header').removeClass('active-click');
+   //  history.pushState('', document.title, window.location.pathname + window.location.search);
+
+    } else {
+      $(lineItemG).children().children().addClass('active-click');
+      $(cardHeader + ' > span' ).addClass('rotate-arrow-to-up');
+   //   history.pushState('', '', sectionID)
+    }
+
+    $(section).on('shown.bs.collapse', function(){
+      let pos = document.querySelector(sectionID).offsetTop;
+      smoothScrollTo(0, pos, 500); 
+    })
   }
 
-  $(section).on('shown.bs.collapse', function(event){
-    
-    var navHeight = $('#header-pinned > div.title').outerHeight();
-    window.scroll({
-      top: $(cardHeader).offset().top - navHeight,
-      behavior: 'smooth'
-    })
+  $('.btn-link').on('click', function(event){
+    event.preventDefault();
+    var formCat = this.getAttribute('data-target').replace('-section','')
+    clickHilite(formCat);
   })
-}
+   
+  $('.form-link').on('click', function(event){
+    event.preventDefault();
+    var formCat = '#' + this.getAttribute('data-target').replace('-section', '')
+    clickHilite(formCat);
+  });
 
-$('.btn-link').on('click', function(event){
-  var formCat = this.getAttribute('data-target').replace('-section','')
-  clickHilite(formCat, event);
-})
- 
-$('.form-link').on('click', function(event){
-  var formCat = '#' + this.getAttribute('data-target').replace('-section', '')
-  clickHilite(formCat, event);
+  // function print() {
+  //     // getting the tag element I want to print
+  //     // cloning the content so it doesn't get messed
+  //     // remove all the possible scripts that could be embed
+  //     var printContents = $('body').clone().find('script').remove().end().html();
+
+  //     // get all <links> and remove all the <script>'s from the header that could run on the new window
+  //     var allLinks = $('head').clone().find('script').remove().end().html();
+
+  //     // open a new window
+  //     var popupWin = window.open('', '_blank');
+  //     // ready for writing
+  //     popupWin.document.open();
+
+  //     // -webkit-print-color-adjust to keep colors for the printing version
+  //     var keepColors = '<style>body {-webkit-print-color-adjust: exact !important; }</style>';
+
+  //     // writing
+  //     // onload="window.print()" to print straigthaway
+  //     popupWin.document.write('<html><head>' + keepColors + allLinks + '</head><body onload="window.print()">' + printContents + '</body></html>');
+
+  //     // close for writing
+  //     popupWin.document.close();
+
+
 });
-
-
-
 
 
 
